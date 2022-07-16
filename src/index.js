f3d2d3139c670d0b515ed78107bf4f5ff5a6dbb4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-16 12:10:41 -0500

    [+] Redux Store: Crea store y realiza algunas acciones sobre este

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const redux = require( 'redux' );
+const createStore = redux.createStore;
+
 // ! Defines the action name as a constant
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
@@ -27,3 +30,23 @@ const reducer = ( state = initialState, action ) => {
             return state;
     }
 }
+
+// ! Redux store
+// ? Responsabilidad #1: Mantiene el estado de la aplicacion
+const store = createStore( reducer );   // ? Crea el almacen de datos de Redux
+
+// ? Responsabilidad #2: Permite acceder al estado global de la aplicacion
+console.log( 'Initial state: ', store.getState() );     
+
+// ? Responsabilidad #3: Registra o subscrible los listeners
+const unsubscribe = store.subscribe( () =>
+    console.log( 'Update state: ', store.getState() )
+);     
+
+// ? Responsabilidad #4: Permite la actualizacion del estado a traves del envio de acciones al Redux Store a traves del metodo dispatch()
+store.dispatch( orderCake() );
+store.dispatch( orderCake() );
+store.dispatch( orderCake() );
+
+// ? Responsabilidad #5: Cancela registro o rechazar subscripcion de los listeners
+unsubscribe();
