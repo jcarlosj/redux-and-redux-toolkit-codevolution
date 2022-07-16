c70cbe03e9c55a89b7d12910ff83f724d7e43978
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-16 11:08:06 -0500

    [+] Action (CAKE_ORDERED): Le da nombre y define la accion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log( 'Saludos desde el index.js' );
+// ! Defines the action name as a constant
+const CAKE_ORDERED = 'CAKE_ORDERED';
+
+// ! Action creator: is a function that returns an action
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1
+    }
+}
