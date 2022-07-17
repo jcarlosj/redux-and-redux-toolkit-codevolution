a060a9bf060301c7e25b116a150a0b007072c0d3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-17 16:40:34 -0500

    [+] async-actions: Define accion asyncrona para realizar una peticion a una API (http://jsonplaceholder.typicode.com/users) usando thunk y axios

--- src/async-actions.js
+++ src/async-actions.js
@@ -1,4 +1,6 @@
-const { createStore } = require( 'redux' );
+const { createStore, applyMiddleware } = require( 'redux' );
+const thunkMiddleware = require( 'redux-thunk' ).default;
+const axios = require( 'axios' );
 
 
 // ! Defines the action name as a constant
@@ -61,6 +63,50 @@ const reducer = ( state = initialState, action ) => {
 
 }
 
+// ! Define async action creator: an action creator returns an action
+const fetchUsers = () => {   
+    // ? Retorna una funcion que no es pura y permite efectos secundarios como realizar peticiones a una API
+    return function( dispatch ) {                           // ? Esto es posible por que recibe el método de envio de acciones como argumento, es decir el 'dispatch'
+        dispatch( fetchUsersRequest() );                    // ? Cambia el estado cuando se esta por hacer la peticion
+
+        axios
+            .get( 'https://jsonplaceholder.typicode.com/usersss' )
+            .then( response => {
+                const users = response.data.map( user => ({
+                    name: user.name,
+                    email: user.email
+                }));
+                // response.data: son los usuarios
+                console.log( users );
+
+                dispatch( fetchUsersSuccess( users ) );     // ? Cambia el estado cuando la peticion es exitosa
+            })
+            .catch( error => {
+                // error.message: es el mensaje de error
+                // console.error( error.message );
+
+                dispatch( fetchUsersFailure( error.message ) );     // ? Cambia el estado cuando la peticion es falla
+            });
+
+
+    }
+}
+
 // ! Redux store
 // ? Responsabilidad #1: Mantiene el estado de la aplicacion
-const store = createStore( reducer );   // ? Crea el almacen de datos de Redux
+const store = createStore( reducer, applyMiddleware( thunkMiddleware ) );   // ? Crea el almacen de datos de Redux e implementa Thunk como Middleware para crear acciones asincronas en Redux
+
+// ? Responsabilidad #2: Permite acceder al estado global de la aplicacion
+console.log( 'Initial state: ', store.getState() );     
+
+// ? Responsabilidad #3: Registra o subscrible los listeners
+const unsubscribe = store.subscribe( () => {
+    console.log( 'Update: ', store.getState() );
+});  
+
+// ? Responsabilidad #4: Permite la actualizacion del estado a traves del envio de acciones al Redux Store a traves del metodo dispatch()
+store.dispatch( fetchUsers() );       // ? Envia una accion asincrona que hemos definido usando thunk
+
+
+// ? Responsabilidad #5: Cancela registro o rechazar subscripcion de los listeners
+// unsubscribe();
