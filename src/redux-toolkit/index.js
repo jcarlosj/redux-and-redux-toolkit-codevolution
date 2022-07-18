9ae418e97c8337cf8faba09de8d941d64470ae77
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-18 18:14:54 -0500

    [+] index: Subscribe/Unsubscribe app al store de Redux & dispatch 'icecream' actions

--- src/redux-toolkit/index.js
+++ src/redux-toolkit/index.js
@@ -1,5 +1,6 @@
 const store = require( './app/store' );
 const cakeActions = require( './features/cake/cakeSlice' ).cakeActions;
+const icecreamActions = require( './features/icecream/icecreamSlice' ).icecreamActions;
 
 // ? Responsabilidad #2: Permite acceder al estado global de la aplicacion
 console.log( 'Initial state: ', store.getState() );
@@ -14,6 +15,10 @@ store.dispatch( cakeActions.ordered() );
 store.dispatch( cakeActions.ordered() );
 store.dispatch( cakeActions.ordered() );
 store.dispatch( cakeActions.restocked( 3 ) );
+
+store.dispatch( icecreamActions.ordered() );
+store.dispatch( icecreamActions.ordered( 2 ) );
+store.dispatch( icecreamActions.restocked( 3 ) );
 
 // ? Responsabilidad #5: Cancela registro o rechazar subscripcion de los listeners
 unsubscribe();
