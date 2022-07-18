4851de6ead017dbe11a6d74d294f377e83af57d9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-18 18:14:14 -0500

    [+] Store: Configura el Store de la aplicacion e implementa nuevo reducer

--- src/redux-toolkit/app/store.js
+++ src/redux-toolkit/app/store.js
@@ -1,13 +1,15 @@
 const configureStore = require( '@reduxjs/toolkit' ).configureStore;
 
 const cakeReducer = require( '../features/cake/cakeSlice' );
+const icecreamReducer = require( '../features/icecream/icecreamSlice' );
 
 // ! Redux store
 // ? Responsabilidad #1: Mantiene el estado de la aplicacion
 const store = configureStore({
     // ! Combine Reducers
     reducer: {
-        cake: cakeReducer
+        cake: cakeReducer,
+        icecream: icecreamReducer
     },
 });
 
