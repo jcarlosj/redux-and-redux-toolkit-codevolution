346518e2a22ed9f9b915796fa88951cfe50d2222
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-07-18 17:52:30 -0500

    [+] index: Subscribe/Unsubscribe app al store de Redux & dispatch 'cake' actions

---
+++ src/redux-toolkit/index.js
@@ -0,0 +1,19 @@
+const store = require( './app/store' );
+const cakeActions = require( './features/cake/cakeSlice' ).cakeActions;
+
+// ? Responsabilidad #2: Permite acceder al estado global de la aplicacion
+console.log( 'Initial state: ', store.getState() );
+
+// ? Responsabilidad #3: Registra o subscrible los listeners
+const unsubscribe = store.subscribe( () => {
+    console.log( 'Update: ', store.getState() );
+}); 
+
+// ? Responsabilidad #4: Permite la actualizacion del estado a traves del envio de acciones al Redux Store a traves del metodo dispatch()
+store.dispatch( cakeActions.ordered() );
+store.dispatch( cakeActions.ordered() );
+store.dispatch( cakeActions.ordered() );
+store.dispatch( cakeActions.restocked( 3 ) );
+
+// ? Responsabilidad #5: Cancela registro o rechazar subscripcion de los listeners
+unsubscribe();
